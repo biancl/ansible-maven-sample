8977640f8af2258ab490b4d9e4df5d07be81afd6
占翼 <13795260@qq.com>
2017-11-15 16:46:20 +0800

    init

---
+++ src/main/java/com/cfets/devops/AnsibleHelloWorldApplication.java
@@ -0,0 +1,12 @@
+package com.cfets.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnsibleHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnsibleHelloWorldApplication.class, args);
+	}
+}
