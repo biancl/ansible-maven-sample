20b12d54254010ab056a611efad51f2266ee6e69
卞乘龙 <bianshenglong_sr@scm.cfets.com>
2018-02-24 09:48:52 +0800

    Update HelloWorldController.java

--- ansible-maven-sample/src/main/java/com/cfets/devops/rest/HelloWorldController.java
+++ ansible-maven-sample/src/main/java/com/cfets/devops/rest/HelloWorldController.java
@@ -1,1 +1,14 @@
-package com.
+package com.cfets.devops.rest;
+
+import org.springframework.web.bind.annoation.PathVariable;
+import org.springframework.web.bind.annoation.RequestMapping;
+import org.springframework.web.bind.annoation.RestController;
+
+@RestController
+public Class HelloWorldController {
+    
+    @RequestMapping(value="helloWorld/{name}")
+    public void helloWorld(@PathVariable String name){
+        return name;
+    }
+}
